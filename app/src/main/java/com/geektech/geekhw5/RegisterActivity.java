231a55fe5d45dd5c002d3df7d14954b076935a34
NPaugust <72886935+NPaugust@users.noreply.github.com>
2022-07-22 15:39:33 +0600

    Initial commit

---
+++ app/src/main/java/com/geektech/geekhw5/RegisterActivity.java
@@ -0,0 +1,18 @@
+package com.geektech.geekhw5;
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegisterActivity extends AppCompatActivity {
+
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        setContentView(R.layout.register);
+
+
+    }
+}
